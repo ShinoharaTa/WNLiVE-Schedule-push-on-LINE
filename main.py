9e2d52da6d25a0da651067750208f0b7aa588ea9
T.Shinohara <takuma1201@gmail.com>
2021-03-16 14:01:25 +0900

    いろいろ細かいところを修正

--- main.py
+++ main.py
@@ -1,31 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import settings
 import datetime
 import schedule
 import time
 import requests
 import logging
+import yaml
+import re
 
 formatter = '%(levelname)s : %(asctime)s : %(message)s'
 logging.basicConfig(filename='logs/log.log', level=logging.DEBUG, format=formatter)
-
-LINE_TOKEN = settings.LINE_TOKEN
-
-casters_master = {
-    "hiyama2018": "檜山 沙耶",
-    "matsu": "松雪 彩花",
-    "ayame": "武藤 彩芽",
-    "komaki2018": "駒木 結衣",
-    "shirai": "白井 ゆかり",
-    "nao": "角田 奈緒子",
-    "yuki": "内田 侑希",
-    "ailin": "山岸 愛梨",
-    "sayane": "江川 清音",
-    "izumin": "眞家 泉",
-    "takayama": "高山 奈々",
-}
 
 def main():
     try:
@@ -34,30 +19,37 @@ def main():
         message = '\n'
         for timetable in timetables:
             if timetable['caster'] != '':
-                if timetable['caster'] in casters_master:
-                    caster = casters_master[timetable['caster']]
+                if timetable['caster'] in config['announcer']:
+                    caster = config['announcer'][timetable['caster']]['name']
                 else:
                     caster = timetable['caster']
             else:
                 caster = "公式チャンネル"
-            message += "■" + timetable['title'] + "\n"
+            message += "■" + timetable['title'].replace("ウェザーニュース", "") + "\n"
             message += "　" + timetable['hour'] + "～ （"+ caster+"）\n\n"
+
+        message = re.sub('\n\n$', '', message)
         send_line_notify(message)
     except:
         logging.error('error')
 
 def send_line_notify(notification_message):
-    line_notify_token = LINE_TOKEN
+    line_notify_token = config['line_token']
     line_notify_api = 'https://notify-api.line.me/api/notify'
     headers = {'Authorization': f'Bearer {line_notify_token}'}
     data = {'message': f'{notification_message}'}
     requests.post(line_notify_api, headers = headers, data = data)
 
 if __name__ == "__main__":
-    schedule.every().day.at("07:00").do(main)
+    with open('config.yaml', 'r', encoding='UTF-8') as file:
+        config = yaml.safe_load(file)
+
+    for notification_time in config['notification_time']:
+        schedule.every().day.at(notification_time).do(main)
+
     while True:
         schedule.run_pending()
-        time.sleep(600)
+        time.sleep(30)
 
     # main()
 
